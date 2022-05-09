2c627484f6dc6d875b9a9c4915c0d663fb4f400f
Ismael Assunçao Costa <maelcosta96@gmail.com>
2022-05-08 21:18:22 -0300

    feat: persisting data at local storage

--- src/store/index.js
+++ src/store/index.js
@@ -1,5 +1,10 @@
 import Vue from "vue";
 import Vuex from "vuex";
+import VuexPersistence from "vuex-persist";
+
+const vuexLocal = new VuexPersistence({
+  storage: window.localStorage,
+});
 
 Vue.use(Vuex);
 
@@ -38,4 +43,5 @@ export default new Vuex.Store({
   },
   actions: {},
   modules: {},
+  plugins: [vuexLocal.plugin],
 });
